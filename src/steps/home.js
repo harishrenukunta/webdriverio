951ab1647a6de5b14de2e1994416fe379d23a56e
harishrenukunta <harish_r9@yahoo.co.uk>
2019-08-08 07:27:57 +0100

    Updated MortgageRatesPage

--- src/steps/home.js
+++ src/steps/home.js
@@ -1,5 +1,7 @@
 import {Given, When} from 'cucumber';
 import home from '../support/pages/home.po'
+
+let mortgageRatesPage;
 Given(
     /^I open the (url|site) "([^"]*)?"$/, function(urlType, url){
         let navigateTo;
@@ -17,12 +19,14 @@ Given(
 
 When(/^navigate to new mortgage rates page$/, function(){
     console.log('Navigated to mortgages page');
-    home.goto('New Mortgage Rates');
+    mortgageRatesPage = home.goto('New Mortgage Rates');
     browser.pause(2000);
 
 });
 
 When(/^find mortgage rate by providing below information:$/, function(mortgageInfoTable){
     const mortgageInfoMap = mortgageInfoTable.hashes();
-    console.log(`Exists:${typeof mortgageInfoMap}`);
+    mortgageRatesPage
+        .haveNationWideMortgage(mortgageInfoMap.MortgageExists);
+    
 });
--- src/support/pages/MortgageRates.po.js
+++ src/support/pages/MortgageRates.po.js
@@ -4,10 +4,27 @@ class MortgageRatesPage{
        // this._yesMortgageExist = $('#selectorItemHaveNationwideMortgage0');
     }
 
+    get yesMortgageExist(){
+        return $('#selectorItemHaveNationwideMortgage0');
+    }
+    get noMortgageExist(){
+        return $('#selectorItemHaveNationwideMortgage1');
+    }
+
     haveNationWideMortgage(isMortgagePresent){
-            isMortgagePresent === 'Yes' ? this._yesMortgageExist.click() : this._noMortgageExist.click();
+            if(isMortgagePresent === 'Yes'){
+                this.yesMortgageExist.click();
+            }else{
+                console.log('No mortgage click');
+                this.noMortgageExist.click();
+            } 
             return this;
     }
+
+    
+
+
+
 }
 
 export default new MortgageRatesPage();
